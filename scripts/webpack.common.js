b963357340df6b1b47c3472b19baf015e4a05d2c
Sefiane Chouaib <sefian.chouaib@gmail.com>
2022-09-26 17:32:00 +0100

    webpack config

---
+++ scripts/webpack.common.js
@@ -0,0 +1,44 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        tartib: './src/js/index.js'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].min.css'
+        }),
+        new webpack.DefinePlugin({
+            VERSION: JSON.stringify(require('../package.json').version)
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s?css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    output: {
+        filename: 'js/[name].min.js',
+        library: {
+            type: 'umd',
+            name: 'Tartib',
+            export: 'default',
+            umdNamedDefine: true
+        }
+    }
+}
