4da7d39d1184a299064a6a4ed1905488081bbe09
Sefiane Chouaib <sefian.chouaib@gmail.com>
2022-09-27 17:20:47 +0100

    get an  element

---
+++ src/js/utils/dom.js
@@ -0,0 +1,14 @@
+import { ROOT } from "../constants";
+import { isString } from "./util";
+
+/**
+ * Gets a DOM element.
+ *
+ * @param {String|Element} ref - CSS selector or DOM element.
+ * @param {Element} context - Element to search from.
+ * @param {Boolean} all - Get all elements.
+ * @returns {Element|null}
+ */
+export const getElement = (ref, context, all) => isString(ref) ? ref && (context || ROOT)['querySelector' + (all ? 'All' : '')](ref)
+                                : ref instanceof Element ? ref
+                                : null;
