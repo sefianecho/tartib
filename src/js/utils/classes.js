189e90038a61aa782d4869b6c468d161a9784117
Sefiane Chouaib <sefian.chouaib@gmail.com>
2022-10-02 14:03:33 +0100

    group classes in a file

---
+++ src/js/utils/classes.js
@@ -0,0 +1,6 @@
+export const TARTIB_CLASSNAME = 'tartib';
+export const ITEM_CLASSNAME = 'tartib__item';
+export const ITEM_SELECTOR = '.' + ITEM_CLASSNAME;
+export const ITEM_DRAGGED_CLASSNAME = ITEM_CLASSNAME + '--dragged';
+export const ELEVATION_CLASSNAME = 'tartib--elevation';
+export const PLACEHOLDER_CLASSNAME = 'tartib__placeholder';
