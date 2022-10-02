1ad4ef4e2168614afd5ba7858c065cfe01aef1e3
Sefiane Chouaib <sefian.chouaib@gmail.com>
2022-10-02 23:02:43 +0100

    remove argument. never used

--- src/js/utils/object.js
+++ src/js/utils/object.js
@@ -8,7 +8,7 @@
 export const objectIterator = (object, fn) => {
     for (const key in object) {
         if (Object.hasOwnProperty.call(object, key)) {
-            fn(object[key], key, object);
+            fn(object[key], key);
         }
     }
 }
