9b39a81e4168f9b805742e6e47907c5b2442a8df
Sefiane Chouaib <sefian.chouaib@gmail.com>
2022-10-04 21:46:00 +0100

    optimization

--- scripts/webpack.prod.js
+++ scripts/webpack.prod.js
@@ -1,7 +1,22 @@
 const { merge } = require('webpack-merge');
+const TerserPlugin = require("terser-webpack-plugin");
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
     mode: 'production',
     devtool: 'source-map',
+
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+            terserOptions: {
+                mangle: {
+                    properties: {
+                        regex: /^_/,
+                        reserved: ['_e', '_s'],
+                    }
+                }
+            }
+        })]
+    }
 });
