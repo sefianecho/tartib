5cbd82a3ea3ec1e9d7e481db4915b428f8f4ddbb
Sefiane Chouaib <sefian.chouaib@gmail.com>
2022-10-04 21:51:41 +0100

    update docs

--- src/js/utils/dragPoint.js
+++ src/js/utils/dragPoint.js
@@ -10,6 +10,8 @@ import { isString } from "./util";
  * @param {Element} item    - Dragged item.
  * @param {Ojbect} dragFrom - Pointer position relative to the dragged item.
  * @param {Object} pointer    - Pointer position.
+ * @param {Element} handle - Drag handle.
+ * @param {Boolean} isRTL - Whether Scrollable is in rtl direction.
  * @returns {Object}
  */
 export const getDragPoint = (item, dragFrom, pointer, handle, isRTL) => {
@@ -24,12 +26,15 @@ export const getDragPoint = (item, dragFrom, pointer, handle, isRTL) => {
         dragVal = dragFrom[axis];
 
         if (dragVal === '' || isNaN(dragVal)) {
+            // If it's a percentage.
             if (isString(dragVal) && /^\d*.?\d+\s*%$/.test(dragVal.trim())) {
                 dragVal = parseFloat(dragVal) * bounds[METHODS[axis]._dimension] / 100;
             } else {
+                // Invalid value.
                 dragVal = null;
             }
         } else {
+            // Number or a String number, convert it to number.
             dragVal *= 1;
         }
 
